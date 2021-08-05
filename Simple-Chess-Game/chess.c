f266a359ccc048c576c8f4e3cc7e94364bb2d846
EinfachBeez <gmal48709@gmail.com>
2021-08-05 14:27:43 +0200

    added first figure (pawn), added checkType, added setPosition, better understandings, playable, added colors, added pawnvalue

--- Simple-Chess-Game/chess.c
+++ Simple-Chess-Game/chess.c
@@ -8,6 +8,8 @@
 // Define Color Codes for better understanding
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 
 // 2D Array of a 8x8 chess board
@@ -22,19 +24,10 @@ char board[8][8] = {
 	{ 'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'}
 };
 
-// Enum of all figure types
-enum Type {
-	PAWN, // 0
-	ROOK, // 1
-	BISHOP, // 2
-	NIGHT, // 3
-	QUEEN, // 4
-	KING, // 5
-};
-
-
 // Define all variables
 bool current_player_white = false;
+int pawnvalue[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int pawnblackvalue[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
 void clearInputQueue()
 {
@@ -91,8 +84,46 @@ bool charIsUpperCase(char ch) {
 	else return false;
 }
 
-void pawn(void) {
+bool pawn(int* target_position, int* start_position, char chosen_figure) {
+	// Only can move one square forward
+	bool isLegal = false;
 
+	//start position axis
+	int start_index_x = start_position[1];
+	int start_index_y = start_position[0];
+	//target position axis
+	int target_index_x = target_position[1];
+	int target_index_y = target_position[0];
+
+	if (current_player_white == true) {
+		// Counts up the pawnvalue to check if this move is the first pawn move 
+		pawnvalue[start_index_x]++;
+		// Checks if the pawnvalue is 1. When the value is 1 it's the first pawn move
+		if (pawnvalue[start_index_x] == 1) {
+			if ((target_index_y - 1 == start_index_y || (target_index_y - 2 == start_index_y))) {
+				isLegal = true;
+			}
+		} else {
+			if (target_index_y - 1 == start_index_y) {
+				isLegal = true;
+			}
+		}
+	} else {
+		// Counts up the pawnblackvalue to check if this move is the first pawn move
+		pawnblackvalue[start_index_x]++;
+		// Checks if the pawnblackvalue is 1. When the value is 1 it's the first pawn move
+		if (pawnblackvalue[start_index_x] == 1) {
+			if ((target_index_y + 1 == start_index_y) || (target_index_y + 2 == start_index_y)) {
+				isLegal = true;
+			}
+		} else {
+			if (target_index_y + 1 == start_index_y) {
+				isLegal = true;
+			}
+		}
+	}
+
+	return isLegal;
 }
 
 void rook(void) {
@@ -143,7 +174,7 @@ again:
 }
 
 
-void getCurrentPosition(int* current_position, int current_position_length) {
+void chooseCurrentPosition(int* current_position, int current_position_length) {
 	// Get the current position which the player has selected
 	int i, k = 0;
 	char current_position_input[4];
@@ -192,36 +223,36 @@ bool isYourColor(char chosen_figure) {
 
 	// Check's if the chosen position exist. When not return
 	if (isEmptySpace(chosen_figure)) {
-		printf("Chosen figure %c doesn't exist\n", chosen_figure);
+		printf("Chosen figure " ANSI_COLOR_RED "doesn't exist\n" ANSI_COLOR_RESET);
 		return false;
 	}
 	// Check if the current player is white
 	if (current_player_white == true) {
 		if (charIsUpperCase(chosen_figure)) {
 			//I am white and the figure is also white
-			printf("Chosen figure %c is your color white.\n", chosen_figure);
+			printf("Chosen figure " ANSI_COLOR_MAGENTA "%c " ANSI_COLOR_GREEN "is " ANSI_COLOR_RESET "your color white.\n", chosen_figure);
 			return true;
 		}
 		else {
 			//I am white but chosen figure is black..
-			printf("Chosen figure %c is NOT your color white.\n", chosen_figure);
+			printf("Chosen figure " ANSI_COLOR_MAGENTA "%c " ANSI_COLOR_RED "is NOT " ANSI_COLOR_RESET "your color white.\n" ANSI_COLOR_RESET, chosen_figure);
 			return false;
 		}
 	} else {
 		if (!charIsUpperCase(chosen_figure)) {
 			//I am black and the figure is also black
-			printf("Chosen figure %c is your color black.\n", chosen_figure);
+			printf("Chosen figure " ANSI_COLOR_CYAN "%c " ANSI_COLOR_GREEN "is " ANSI_COLOR_RESET "your color black.\n", chosen_figure);
 			return true;
 		} else {
 			//I am black but chosen figure is white..
-			printf("Chosen figure %c is NOT your color black.\n", chosen_figure);
+			printf("Chosen figure " ANSI_COLOR_CYAN "%c " ANSI_COLOR_RED "is NOT " ANSI_COLOR_RESET "your color black.\n", chosen_figure);
 			return false;
 		}
 	}
 
 }
 
-void getPosition(int* position, int position_length, char choosen_figure) {
+void chooseMovePosition(int* position, int position_length, char choosen_figure) {
 	// Get the position on which field the player wants to have his figure
 	//return position;
 	int i, k = 0;
@@ -251,19 +282,72 @@ void getPosition(int* position, int position_length, char choosen_figure) {
 			}
 		}
 	}
-
+	printf("\n");
 }
 
-void getType() {
+bool checkType(int* target_position, int* start_position, char chosen_figure) {
 	// Get the type of this figure from getCurrentPosition() (King, Queen, ...)
+	// and check whether it is allowed to move to target position
+	
+	bool isPossible = false;
+
+	// Switch to x and y values from input
+	switch (chosen_figure) {
+	// Pawn White
+	case 'P':
+	// Pawn Black
+	case 'p':
+		isPossible = pawn(target_position, start_position, chosen_figure);
+		break;
+	// Rook White
+	case 'R':
+		break;
+	// Night White
+	case 'N':
+		break;
+	// Bishop White
+	case 'B':
+		break;
+	// King White
+	case 'K':
+		break;
+	// Queen White
+	case 'Q':
+		break;
+	// Rook Black
+	case 'r':
+		break;
+	// Night Black
+	case 'n':
+		break;
+	// Bishop Black
+	case 'b':
+		break;
+	// King Black
+	case 'k':
+		break;
+	// Queen Black
+	case 'q':
+		break;
+	default:
+		break;
+	}
+
+	return isPossible;
 }
 
-void isPossible() {
-	// Checks if this move is possible
-}
-
-void setPosition(char position) {
+void setPosition(int* target_position, int* start_position, char chosen_figure) {
 	// Set the position from the figure the player choose
+
+	//start position axis
+	int start_index_x = start_position[1];
+	int start_index_y = start_position[0];
+	//target position axis
+	int target_index_x = target_position[1];
+	int target_index_y = target_position[0];
+
+	board[start_index_x][start_index_y] = ' ';
+	board[target_index_x][target_index_y] = chosen_figure;
 }
 
 void getCurrentColor() {
@@ -271,9 +355,9 @@ void getCurrentColor() {
 	current_player_white = !current_player_white;
 
 	if (current_player_white)
-		printf("\nIt's whites turn!\n");
+		printf("\nIt's " ANSI_COLOR_MAGENTA "whites " ANSI_COLOR_RESET "turn!\n");
 	else
-		printf("\nIt's blacks turn!\n");
+		printf("\nIt's " ANSI_COLOR_CYAN "blacks " ANSI_COLOR_RESET "turn!\n");
 } 
 
 int main(void) {
@@ -281,12 +365,13 @@ int main(void) {
 	int current_position[2] = { 0 };
 	int current_position_length = sizeof(current_position) / sizeof(current_position[0]);
 
-	int position[2] = { 0 };
-	int position_length = sizeof(position) / sizeof(position[0]);
+	int target_position[2] = { 0 };
+	int position_length = sizeof(target_position) / sizeof(target_position[0]);
 
 	int index_x = -1;
 	int index_y = -1;
 	char chosen_figure = ' ';
+	bool isPossible = false;
 
 	print_board(); // Print the default chess board from sratch
 	
@@ -295,25 +380,33 @@ int main(void) {
 	do {
 		// Loop to repeat all chess relevant tasks
 
-		getCurrentPosition(current_position, current_position_length);
+			//todo pack in do while bis player die richtige Figur ausgewählt hat
+			
+		do {
 
-		index_x = current_position[1];
-		index_y = current_position[0];
-		chosen_figure = board[index_x][index_y];
+			chooseCurrentPosition(current_position, current_position_length);
 
-		isYourColor(chosen_figure);
+			index_x = current_position[1];
+			index_y = current_position[0];
+			chosen_figure = board[index_x][index_y];
 
-		getPosition(position, position_length, chosen_figure);
+		} while (isYourColor(chosen_figure) == false);
 
-		getType();
+		do {
 
-		isPossible();
+			chooseMovePosition(target_position, position_length, chosen_figure);
 
-		/*setPosition(); */
+			isPossible = checkType(target_position, current_position, chosen_figure);
+
+		} while (isPossible == true);
+
+		setPosition(target_position, current_position, chosen_figure);
+
+		print_board(); // Print the default chess board from sratch
 
 		getCurrentColor();
 
-		print_board(); // Print the default chess board from sratch
+		clearInputQueue();
 		
 	// When end or win
 	} while (end == false);
